42b6b4f0b11c018c58e7f24fc609b93db0c09f9a
Zamin Ahmadi <asam79961@gmail.com>
2024-07-23 22:26:09 +1000

    Task1.2P

---
+++ Module 1/Task1.2P/T2.P.ino
@@ -0,0 +1,32 @@
+// Define the pin for the button
+const uint8_t BTN_PIN = 2;
+// Define the pin for the LED
+const uint8_t LED_PIN = 13;
+
+// Variable to store the current state of the LED
+uint8_t ledState = LOW;
+
+
+void handleLED(){
+  ledState = !ledState;
+  digitalWrite(LED_PIN, ledState);
+  Serial.print("LED state: ");
+  Serial.println(ledState);
+}
+
+void setup()
+{
+  // Set the button pin as input with an internal pull-up resistor
+  pinMode(BTN_PIN, INPUT_PULLUP);
+  // Set the LED pin as output
+  pinMode(LED_PIN, OUTPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(BTN_PIN), handleLED, CHANGE);
+  
+  // Initialize serial communication at a baud rate of 9600
+  Serial.begin(9600);
+}
+
+void loop()
+{
+}
